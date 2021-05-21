8345b3fc70505d90a33c34725e63db3ab737e8d2
Pavlo Chaikivskyi <cajkovskijpavel5@gmail.com>
2021-05-21 15:51:12 +0200

    111

---
+++ main.js
@@ -0,0 +1,6 @@
+function click() {
+    let input1 = document.getElementById('input1').value
+    let input2 = document.getElementById('input2').value
+    let click = input1 + input2
+    console.log(click)
+}
